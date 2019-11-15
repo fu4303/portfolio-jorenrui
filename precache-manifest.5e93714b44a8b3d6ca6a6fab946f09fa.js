239d3c4b8d334ea0e1d398067ad45af50ca8d6cf
Joeylene Rivera <joeylenerivera@gmail.com>
2019-11-15 18:29:13 +0800

    Updates

---
+++ precache-manifest.5e93714b44a8b3d6ca6a6fab946f09fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4d06d229a862bb7e4d433789128c08",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f8464a3889de3a942112",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e3af08a6a4065765d373",
+    "url": "/portfolio/static/css/main.94f3949c.chunk.css"
+  },
+  {
+    "revision": "f8464a3889de3a942112",
+    "url": "/portfolio/static/js/2.64254ca2.chunk.js"
+  },
+  {
+    "revision": "e3af08a6a4065765d373",
+    "url": "/portfolio/static/js/main.c433a012.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
